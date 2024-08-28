28b2d7f9c70328fa77e063ca55b51dffa690ca9e
AkshataSolapurkar <solapurkarakshata@gmail.com>
2024-08-28 16:25:15 +0530

    / updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    router.replace('/mainstore');
+    router.replace('/adminpannel/dashboard');
   }, [router]);
 
   return null;
